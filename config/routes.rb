56421ef3e83b39ec51d72a44bce1c37472f9beaf
Josenilton Duarte <n8zion@gmail.com>
2022-11-15 13:09:28 -0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#show'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
